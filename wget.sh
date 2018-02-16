f62308734fc150df0f55af266eda7ff076073b68
T3CH-SUPP0RT <karanbir.ahuja@gmail.com>
2018-02-16 15:41:57 -0800

    Create wget.sh

---
+++ wget.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo 'LOL'
+wget -O splunk-7.0.2-03bbabbd5c0f-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.2&product=splunk&filename=splunk-7.0.2-03bbabbd5c0f-linux-2.6-x86_64.rpm&wget=true'
