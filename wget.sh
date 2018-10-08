20124fc48ca645fd16e06f3a8f27d9da6b063b2f
Turb0Yoda <karanbir.ahuja@gmail.com>
2018-10-07 19:56:34 -0700

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo 'LOL'
-wget -O splunk-7.0.2-03bbabbd5c0f-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.2&product=splunk&filename=splunk-7.0.2-03bbabbd5c0f-linux-2.6-x86_64.rpm&wget=true'
+wget -O splunk-7.2.0-8c86330ac18-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.0&product=splunk&filename=splunk-7.2.0-8c86330ac18-linux-2.6-amd64.deb&wget=true'
