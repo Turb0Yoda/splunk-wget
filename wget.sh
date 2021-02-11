d4cfd803c36f95c7a83b32a16fc3b6fba3c13fce
Turb0Yoda <karanbir.ahuja@gmail.com>
2021-02-10 17:51:52 -0800

    Update wget.sh

--- wget.sh
+++ wget.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-echo 'LOL'
-wget -O splunk-7.2.0-8c86330ac18-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.0&product=splunk&filename=splunk-7.2.0-8c86330ac18-linux-2.6-amd64.deb&wget=true'
-
-
-wget -O splunkforwarder-7.2.5.1-962d9a8e1586-linux-2.6-x86_64.rpm 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.2.5.1&product=universalforwarder&filename=splunkforwarder-7.2.5.1-962d9a8e1586-linux-2.6-x86_64.rpm&wget=true'
+echo 'I am lazy ok'
+wget -O splunk-8.1.2-545206cc9f70-linux-2.6-amd64.deb 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=8.1.2&product=splunk&filename=splunk-8.1.2-545206cc9f70-linux-2.6-amd64.deb&wget=true'
